3a89e49bce690d612a06379ffbbd6bc98ea0f8db
Viggiecc <vigiecc@gmail.com>
2018-07-09 13:09:26 -0700

    React- Clock Stateful Component

--- src/index.js
+++ src/index.js
@@ -8,12 +8,26 @@ class Clock extends React.Component {
     super(props);
     this.state = {date: new Date()};
   }
+  componentDidMount() {
+    this.timeID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timeID);
+  }
 
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
   render() {
     return (
       <div className="App">
         <h1>Hello {this.props.name}</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <h2>Current Time is {this.state.date.toLocaleTimeString()}</h2>
       </div>
     );
   }
@@ -23,8 +37,7 @@ function App() {
   return (
     <div>
       <Clock name="React" />
-      <Clock name="React" />
-
+      <Clock name="Angular" />
     </div>
   )
 }
